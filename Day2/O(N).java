3ffc18bea1e56e28bd9b62f2faee9b3eddce8098
Ranbir Singh <106223825+ranbirsingh-max@users.noreply.github.com>
2024-04-13 16:56:43 +0530

    Create O(N).java

---
+++ Day2/O(N).java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void printItems(int n) {
+        for (int i=0; i<n; i++){
+            System.out.println(i);
+        }
+    }
+
+
+
+   
+    public static void main(String[] args) {
+        printItems(10);
+    }
