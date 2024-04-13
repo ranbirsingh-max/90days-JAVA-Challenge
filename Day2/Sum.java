97388e72f6b69342ec851ed124418795dd788fb6
Ranbir Singh <106223825+ranbirsingh-max@users.noreply.github.com>
2024-04-13 11:41:15 +0530

    Create Sum.java

---
+++ Day2/Sum.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=10; byte b=127; int c=10;
+        int sum = a+b+c;
+        System.out.println("Hello world : " + sum);
+    }
+}
