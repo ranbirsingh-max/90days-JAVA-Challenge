dd6f11cc9286926a3c5f09b8b1512c52e66fe645
Ranbir Singh <106223825+ranbirsingh-max@users.noreply.github.com>
2024-04-13 17:52:49 +0530

    Create DropConstant.java

---
+++ Day2/DropConstant.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void items(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
+
+        for(int k=0; k<n; k++){
+            System.out.println(k);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        items(10);
+    }
