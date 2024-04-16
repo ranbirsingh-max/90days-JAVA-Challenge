7e3136b2523183e55cc2d7d4f4e88b27aaca54a2
Ranbir Singh <106223825+ranbirsingh-max@users.noreply.github.com>
2024-04-16 12:33:23 +0530

    Create 4.java

---
+++ Day5/4.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Enter the distance in km: ");
+        Scanner km=new Scanner(System.in);
+        Float a = km.nextFloat();
+        System.out.println(a*0.621371);
+    }
+}
