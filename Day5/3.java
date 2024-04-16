b19edd06d9203eb9282e5c5119cb02f1559a5e59
Ranbir Singh <106223825+ranbirsingh-max@users.noreply.github.com>
2024-04-16 12:32:52 +0530

    Create 3.java

---
+++ Day5/3.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Test{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String src =sc.next();
+        System.out.println("Hello "+ src+"have a good day");
+    }
+}
