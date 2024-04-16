0f1fd76f5d6d67687abb8a0bde81dfd1c76bb5a2
Ranbir Singh <106223825+ranbirsingh-max@users.noreply.github.com>
2024-04-16 12:32:11 +0530

    Create 2.java

---
+++ Day5/2.java
@@ -0,0 +1,11 @@
+public class Test{
+    public static void main(String[] args) {
+        int a=45;
+        int b=48;
+        int c=95;
+        int sum = a+b+c;
+        int total=300;
+        float cgpa= ((float) sum/total)*100;
+        System.out.println(cgpa/9.5);
+    }
+}
