80a0931a37760a018328a0f7c1ff5f1d65959886
Ranbir Singh <106223825+ranbirsingh-max@users.noreply.github.com>
2024-04-16 12:31:35 +0530

    Create PracticeSet.java

---
+++ Day5/PracticeSet.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        int a=10;
+        int b=10;
+        int c=10;
+        int sum= a+b+c;
+        System.out.println(sum);
+
+    }
+}
