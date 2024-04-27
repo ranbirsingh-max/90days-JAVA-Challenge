70a13eb4f4da110c6b7ae109b2d8ba24d1b92c36
Ranbir Singh <106223825+ranbirsingh-max@users.noreply.github.com>
2024-04-28 01:02:13 +0530

    Create logical_operations.java

---
+++ Day10/logical_operations.java
@@ -0,0 +1,23 @@
+public class Video_17 {
+    public static void main(String[] args) {
+        boolean a=true;
+        boolean b=false;
+        System.out.println("Logical AND");
+        if(a && b){
+            System.out.println("True");
+        }
+        else {
+            System.out.println("False");
+        }
+        System.out.println("Logical or");
+        if(a || b){
+            System.out.println("True");
+        }
+        else {
+            System.out.println("False");
+        }
+        System.out.println("Logical NOT");
+        System.out.println(!a);
+        System.out.println(!b);
+    }
+}
