aaac3b127f255ca106824f9f03b8e50cc48581ae
Ranbir Singh <106223825+ranbirsingh-max@users.noreply.github.com>
2024-04-28 22:18:22 +0530

    Create PracticeSet_4.java

---
+++ Day11/PracticeSet_4.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Practice_set4 {
+    public static void main(String[] args) {
+        //Question1
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your First subject marks: ");
+        int num1= sc.nextInt();
+        System.out.println("Enter your second subject marks: ");
+        int num2=sc.nextInt();
+        System.out.println("Enter your third subject marks: ");
+        int num3= sc.nextInt();
+        float sum= (num1+num2+num3)/300.0f * 100;
+        System.out.println(sum);
+        if(sum>40 && num1>=33 && num2>=33 && num3>=33){
+            System.out.println("Pass");
+        }else {
+            System.out.println("Sorry you are fail");
+        }
+
+        //Question 2
+        Scanner scc=new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int day = scc.nextInt();
+        switch (day){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Wrong input");
+        }
+
+        //Problem3
+        Scanner or=new Scanner(System.in);
+        String ne=or.next();
+        if(ne.endsWith(".org")){
+            System.out.println("organisational website");
+        } else if (ne.endsWith(".com")) {
+            System.out.println("commercial website");
+        }else {
+            System.out.println("indian website");
+        }
+
+    }
+}
