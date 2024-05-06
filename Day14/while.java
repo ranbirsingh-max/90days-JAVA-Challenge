b2cd414dcc4aa7065df1698d9f9c155dabe773f7
Ranbir Singh <106223825+ranbirsingh-max@users.noreply.github.com>
2024-05-06 22:52:38 +0530

    Update while.java

--- Day14/while.java
+++ Day14/while.java
@@ -5,5 +5,11 @@ public class video21 {
             System.out.println(i);
             i++;
         }
+        System.out.println("Do While Loop:");
+        int a =0;
+        do{
+            System.out.println(a);
+            a++;
+        }while(a<=50);
     }
 }
