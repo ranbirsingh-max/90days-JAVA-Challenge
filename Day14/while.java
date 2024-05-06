b3e9403672e5bf3b4de2f119de9194204a974b63
Ranbir Singh <106223825+ranbirsingh-max@users.noreply.github.com>
2024-05-06 22:26:40 +0530

    Create while.java

---
+++ Day14/while.java
@@ -0,0 +1,9 @@
+public class video21 {
+    public static void main(String[] args) {
+        int i = 100;
+        while (i <= 200){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
