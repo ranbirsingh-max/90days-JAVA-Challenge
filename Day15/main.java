b67598480e4b822fc598c493b0ea6a343af5f0b5
Ranbir Singh <106223825+ranbirsingh-max@users.noreply.github.com>
2024-05-08 00:21:38 +0530

    Create main.java

---
+++ Day15/main.java
@@ -0,0 +1,18 @@
+package Linked_list;
+
+public class main {
+        public static void main(String[] args) {
+                Linked_list_ my_list=new Linked_list_(6);
+                my_list.getHead();
+                my_list.getTail();
+                my_list.getLength();
+                my_list.append(5);
+
+                my_list.getHead();
+                my_list.getTail();
+                my_list.getLength();
+
+                my_list.printList();
+        }
+
+}
