ec22c41c3d53a21ca6bbcd5ac0784b8767eae448
Ranbir Singh <106223825+ranbirsingh-max@users.noreply.github.com>
2024-10-22 22:09:52 +0530

    Create increment_and_decrement.java

---
+++ Day6/increment_and_decrement.java
@@ -0,0 +1,12 @@
+public class Day_6 {
+    public static void main(String[] args) {
+        // Increment and Decrement operators
+        int i = 10;
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+        char ch = 'y';
+        System.out.println(++ch);
+    }
+}
