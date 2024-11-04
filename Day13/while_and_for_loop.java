7048f6a99a5c9f3bf96ae0ae8e266e7eaa6e851f
Ranbir Singh <106223825+ranbirsingh-max@users.noreply.github.com>
2024-11-04 21:20:11 +0530

    Create while_and_for_loop.java

---
+++ Day13/while_and_for_loop.java
@@ -0,0 +1,24 @@
+public class Day_13 {
+    public static void main(String[] args) {
+        int n = 0;
+        do{
+            System.out.println(n);
+            n++;
+        }while ( n <= 10);
+        for (int n= 0; n <=10; n++){
+            System.out.println(n);
+        }
+
+        for (int n = 0 ; 2*n +1 <=10 ; n++){
+            System.out.println(2*n + 1);
+        }
+        for (int n = 9 ; n != 0  ; n--){
+            System.out.println(n);
+        }
+       int n ;
+        for ( n=10 ; n != 0 ; n--){
+            System.out.println(n);
+        }
+
+    }
+}
