6863bcd5f183681d8d09e5300bc2cb6bfea929ab
Ranbir Singh <106223825+ranbirsingh-max@users.noreply.github.com>
2024-11-30 13:50:57 +0530

    Update practiceSet6.java

--- Day18/practiceSet6.java
+++ Day18/practiceSet6.java
@@ -1,1 +1,40 @@
+import java.util.Arrays;
 
+public class Day_18 {
+    public static void main(String[] args) {
+       int[] array2 = {13, 2, 3, 4, 57};
+       System.out.println(find_max(array2));
+   }
+
+       public static int find_max(int[] array2) {
+           int max = 0;
+           for (int element : array2) {
+               if (element > max) {
+                   max = element;
+               } else {
+
+                   max = max;
+               }
+           }
+           return max;
+       int index=array2[0];
+       for(int e:array2){
+           if(index < e){
+               index=index;
+           }else{
+               index=e;
+           }
+       }
+       System.out.println(index);
+        int[] array3 = {1, 2, 3, 4, 57};
+        boolean sorted= true;
+       for(int i=0;i<array3.length-1;i++){
+           if(array3[i]>array3[i+1]){
+               sorted=false;
+               break;
+           }
+       }
+        System.out.println(sorted);
+        }
+
+    }
